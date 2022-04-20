b98c426e02d5a7cc792cff8c8dc48a89a152fedf
Mr South <inoutmail@gmail.com>
2022-04-20 07:44:48 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <TaskTimerList/>
+  </div>
+</template>
+
+<script>
+import TaskTimerList from "./components/TaskTimerList";
+
+export default {
+  name: "App",
+  components: {
+    TaskTimerList
+  }
+};
+</script>
